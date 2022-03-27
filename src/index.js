fa4dd6c4822d77c08437c1f5d4b48a496b6e1e32
EricFooSD <fooshida@gmail.com>
2022-03-27 12:14:01 +0800

    modify keyboard to be the main input method / trigger of backend calls. css styling

--- src/index.js
+++ src/index.js
@@ -4,71 +4,49 @@
 import './styles.scss';
 /*
  * ========================================================
- *                  HELPER FUNCTIONS
+ *                 GLOBAL VARIABLE
  * ========================================================
  */
 
-const getElement = (id) => document.getElementById(`${id}`);
+let currentGuess = [];
 
 /*
  * ========================================================
- *                  DOM FUNCTIONS
+ *                 DOM HELPER FUNCTIONS
  * ========================================================
  */
+
+const getElement = (id) => document.getElementById(id);
 
 const createRowInBoard = (number, parent) => {
   const newRow = document.createElement('div');
   newRow.classList.add('row');
   newRow.setAttribute('id', `row${number}`);
+  newRow.classList.add('justify-content-center');
   parent.appendChild(newRow);
 
   for (let i = 0; i < 5; i += 1) {
     const letterTile = document.createElement('div');
-    letterTile.classList.add('col-2');
-
-    const input = document.createElement('input');
-    input.setAttribute('id', `row${number}letter${i}`);
-    input.setAttribute('type', 'text');
-    input.setAttribute('class', 'form-control');
-    letterTile.appendChild(input);
-
+    letterTile.setAttribute('id', `R${number}L${i}`);
+    letterTile.classList.add('tile');
+    letterTile.classList.add('flex');
+    letterTile.innerHTML = '';
     newRow.appendChild(letterTile);
   }
-
-  // button
-  const buttonContainer = document.createElement('div');
-  buttonContainer.classList.add('col-2');
-  const submitBtn = document.createElement('button');
-  submitBtn.setAttribute('id', `submit${number}`);
-  submitBtn.innerText = 'Submit';
-
-  submitBtn.addEventListener('click', () => {
-    const guessedWord = `${getElement(`row${number}letter0`).value}`
-    + `${getElement(`row${number}letter1`).value}`
-    + `${getElement(`row${number}letter2`).value}`
-    + `${getElement(`row${number}letter3`).value}`
-    + `${getElement(`row${number}letter4`).value}`;
-
-    console.log('guessedWord', guessedWord);
-
-    axios.post('/checkWord', {
-      guess: guessedWord,
-    })
-      .then((response) => {
-        console.log(response.data);
-        const check = response.data;
-        for (let i = 0; i < 5; i += 1) {
-          getElement(`row${number}letter${i}`).classList.add(`${check.color[i]}`);
-        }
-      })
-      .catch((error) => { console.log(error); });
-  });
-
-  buttonContainer.appendChild(submitBtn);
-  newRow.appendChild(buttonContainer);
-
-  return newRow;
 };
+
+const updateRow = (array) => {
+  const guessNumber = sessionStorage.getItem('guessNumber');
+  for (let i = 0; i < 5; i += 1) {
+    if (!array[i]) getElement(`R${guessNumber}L${i}`).innerHTML = '';
+    else getElement(`R${guessNumber}L${i}`).innerHTML = `${array[i].toUpperCase()}`;
+  }
+};
+/*
+ * ========================================================
+ *                  DOM FUNCTIONS
+ * ========================================================
+ */
 
 const createWordBoard = () => {
   // div for main game board
@@ -77,12 +55,12 @@ const createWordBoard = () => {
   gameBoard.setAttribute('id', 'game-board-container');
   document.body.appendChild(gameBoard);
   // create each row in game board
+  createRowInBoard(0, gameBoard);
   createRowInBoard(1, gameBoard);
   createRowInBoard(2, gameBoard);
   createRowInBoard(3, gameBoard);
   createRowInBoard(4, gameBoard);
   createRowInBoard(5, gameBoard);
-  createRowInBoard(6, gameBoard);
 };
 
 const Keyboard = {
@@ -92,23 +70,13 @@ const Keyboard = {
     keys: [],
   },
 
-  eventHandlers: {
-    oninput: null,
-    onclose: null,
-  },
-
-  properties: {
-    value: '',
-    capsLock: false,
-  },
-
   init() {
     // Create main elements
     this.elements.main = document.createElement('div');
     this.elements.keysContainer = document.createElement('div');
 
     // Setup main elements
-    this.elements.main.classList.add('keyboard', 'keyboard--hidden');
+    this.elements.main.classList.add('keyboard');
     this.elements.keysContainer.classList.add('keyboard__keys');
     this.elements.keysContainer.appendChild(this._createKeys());
 
@@ -117,25 +85,14 @@ const Keyboard = {
     // Add to DOM
     this.elements.main.appendChild(this.elements.keysContainer);
     document.body.appendChild(this.elements.main);
-
-    // Automatically use keyboard for elements with .use-keyboard-input
-    document.querySelectorAll('.use-keyboard-input').forEach((element) => {
-      element.addEventListener('focus', () => {
-        this.open(element.value, (currentValue) => {
-          element.value = currentValue;
-        });
-      });
-    });
   },
 
   _createKeys() {
     const fragment = document.createDocumentFragment();
     const keyLayout = [
-      '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'backspace',
       'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-      'caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'enter',
-      'done', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '?',
-      'space',
+      'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
+      'done', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'backspace',
     ];
 
     // Creates HTML for an icon
@@ -143,7 +100,7 @@ const Keyboard = {
 
     keyLayout.forEach((key) => {
       const keyElement = document.createElement('button');
-      const insertLineBreak = ['backspace', 'p', 'enter', '?'].indexOf(key) !== -1;
+      const insertLineBreak = ['backspace', 'p', 'l'].indexOf(key) !== -1;
 
       // Add attributes/classes
       keyElement.setAttribute('type', 'button');
@@ -155,43 +112,12 @@ const Keyboard = {
           keyElement.innerHTML = createIconHTML('backspace');
 
           keyElement.addEventListener('click', () => {
-            this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
-            this._triggerEvent('oninput');
+            if (currentGuess.length > 0) {
+              currentGuess.pop();
+              console.log('currentGuess', currentGuess);
+              updateRow(currentGuess);
+            }
           });
-
-          break;
-
-        case 'caps':
-          keyElement.classList.add('keyboard__key--wide', 'keyboard__key--activatable');
-          keyElement.innerHTML = createIconHTML('keyboard_capslock');
-
-          keyElement.addEventListener('click', () => {
-            this._toggleCapsLock();
-            keyElement.classList.toggle('keyboard__key--active', this.properties.capsLock);
-          });
-
-          break;
-
-        case 'enter':
-          keyElement.classList.add('keyboard__key--wide');
-          keyElement.innerHTML = createIconHTML('keyboard_return');
-
-          keyElement.addEventListener('click', () => {
-            this.properties.value += '\n';
-            this._triggerEvent('oninput');
-          });
-
-          break;
-
-        case 'space':
-          keyElement.classList.add('keyboard__key--extra-wide');
-          keyElement.innerHTML = createIconHTML('space_bar');
-
-          keyElement.addEventListener('click', () => {
-            this.properties.value += ' ';
-            this._triggerEvent('oninput');
-          });
-
           break;
 
         case 'done':
@@ -199,20 +125,39 @@ const Keyboard = {
           keyElement.innerHTML = createIconHTML('check_circle');
 
           keyElement.addEventListener('click', () => {
-            this.close();
-            this._triggerEvent('onclose');
+            let guess = '';
+            currentGuess.forEach((letter) => {
+              guess += letter;
+            });
+            axios
+              .post('/checkGame', { guess })
+              .then((response) => {
+                console.log(response.data);
+                const check = response.data;
+                if (check.isWord) {
+                  for (let i = 0; i < 5; i += 1) {
+                    const letter = getElement(`R${check.guessNum}L${i}`);
+                    letter.classList.add(`${check.color[i]}`);
+                    letter.innerHTML = `${(check.split[i]).toUpperCase()}`;
+                  }
+                  sessionStorage.setItem('guessNumber', `${check.guessNum + 1}`);
+                  currentGuess = [];
+                }
+              })
+              .catch((error) => { console.log(error); });
           });
-
           break;
 
         default:
-          keyElement.textContent = key.toLowerCase();
+          keyElement.textContent = key.toUpperCase();
 
           keyElement.addEventListener('click', () => {
-            this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
-            this._triggerEvent('oninput');
+            if (currentGuess.length < 5) {
+              currentGuess.push(key);
+              console.log('currentGuess', currentGuess);
+              updateRow(currentGuess);
+            }
           });
-
           break;
       }
 
@@ -226,35 +171,6 @@ const Keyboard = {
     return fragment;
   },
 
-  _triggerEvent(handlerName) {
-    if (typeof this.eventHandlers[handlerName] === 'function') {
-      this.eventHandlers[handlerName](this.properties.value);
-    }
-  },
-
-  _toggleCapsLock() {
-    this.properties.capsLock = !this.properties.capsLock;
-
-    for (const key of this.elements.keys) {
-      if (key.childElementCount === 0) {
-        key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
-      }
-    }
-  },
-
-  open(initialValue, oninput, onclose) {
-    this.properties.value = initialValue || '';
-    this.eventHandlers.oninput = oninput;
-    this.eventHandlers.onclose = onclose;
-    this.elements.main.classList.remove('keyboard--hidden');
-  },
-
-  close() {
-    // this.properties.value = '';
-    // this.eventHandlers.oninput = oninput;
-    // this.eventHandlers.onclose = onclose;
-    // this.elements.main.classList.add('keyboard--hidden');
-  },
 };
 
 /*
@@ -265,8 +181,25 @@ const Keyboard = {
 
 const startGame = () => {
   createWordBoard();
+
+  axios.post('/loadCurrentGame')
+    .then((response) => {
+      console.log(response.data);
+      const current = response.data;
+      for (let i = 0; i < current.guessSplit.length; i += 1) {
+        for (let j = 0; j < 5; j += 1) {
+          const letter = getElement(`R${i}L${j}`);
+          letter.classList.add(`${current.color[i][j]}`);
+          letter.innerHTML = `${(current.guessSplit[i][j]).toUpperCase()}`;
+        }
+      }
+      sessionStorage.setItem('guessNumber', `${current.color.length}`);
+    })
+    .catch((error) => { console.log(error); });
 };
 
 startGame();
-Keyboard.init();
-Keyboard.open();
+
+window.addEventListener('DOMContentLoaded', () => {
+  Keyboard.init();
+});
