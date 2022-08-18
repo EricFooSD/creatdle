ed6b46ce3a474c1483150ba1449c970b8269b0bd
EricFooSD <fooshida@gmail.com>
2022-08-18 17:54:57 +0800

    update comments for games.mjs with JSDoc format

--- controllers/games.mjs
+++ controllers/games.mjs
@@ -1,25 +1,81 @@
-/*
- * ========================================================
- *                  HELPER Functions
- * ========================================================
- */
-
+/* eslint-disable max-len */
 /*
  * ========================================================
  *                  Controller Functions
  * ========================================================
  */
-
 export default function initGamesController(db) {
-  // load current status of game
+  // .....................................
+  // Checking Game States
+  // .....................................
+
+  /**
+ * @desc checking if unique code entered is valid and create game board
+ * @param request unique code entered by user
+ * @param request user ID
+ */
+  const checkCodeAndCreate = async (request, response) => {
+    try {
+      // check if any wordle in Wordle DB has the unique code user entered
+      const { enteredCode } = request.body;
+      const { user } = request.body;
+      const wordle = await db.Wordle.findOne({ where: { code: `${enteredCode}` } });
+
+      // base response object where unique code cannot be found in Wordle DB
+      let responseObj = {
+        availCode: false,
+      };
+      // if there is a corresponding Wordle in Wordle DB, check if user already has an existing game in Games DB
+      if (wordle) {
+        const existingGame = await db.Game.findOne({ where: { playerId: `${user}`, wordleId: `${wordle.id}` } });
+        // if there is no existing game, create game for user in Games DB
+        // else, there is an existing game, send existing game ID to frontend
+        if (!existingGame) {
+          // define new game
+          const newGame = {
+            wordleId: wordle.id,
+            gameState: {
+              words: wordle.words.words,
+              tally: wordle.words.tally,
+              currentWord: 0,
+              guesses: [],
+              color: [],
+            },
+            playerId: user,
+          };
+          // create new game in Games DB
+          const game = await db.Game.create(newGame);
+          responseObj = {
+            availCode: true,
+            gameId: game.id,
+          };
+        } else {
+          responseObj = {
+            availCode: true,
+            gameId: existingGame.id,
+          };
+        }
+      }
+      response.send(responseObj);
+    } catch (error) {
+      response.status(500).send(error);
+    }
+  };
+
+  /**
+ * @desc using game ID pull data about game from DB
+ * @param request game ID
+ */
   const checkCurrentGameState = async (request, response) => {
     const { gameId } = request.body;
     try {
+      // get data on existing game play
       const game = await db.Game.findByPk(gameId);
 
+      // get info on Wordle
       const wordle = await db.Wordle.findByPk(game.wordleId);
 
-      // query the word for this game
+      // define all current guesses and color code of the guesses
       const guessArray = [];
       game.gameState.guesses.forEach((element) => {
         guessArray.push(element.split(''));
@@ -39,10 +95,13 @@ export default function initGamesController(db) {
     }
   };
 
-  // check guess against word
+  /**
+ * @desc to be run when user enters their guess
+ * @param request user's 5 letter guess
+ */
   const checkGuess = async (request, response) => {
     try {
-      // check if guess is even a word DB query
+      // check if guess is a real word from allWords DB
       const { guess } = request.body;
       const isWord = await db.AllWord.findOne({ where: { word: `${guess}` } });
 
@@ -55,12 +114,14 @@ export default function initGamesController(db) {
         split: [],
       };
 
+      // if users guess is not a real word, change response object
       if (!isWord) {
-        // change response object
         responseObj.isWord = false;
-      } else {
+      }
+      // else, users guess is valid, check the guess against answer
+      else {
         const { gameId } = request.body;
-        // query the word for this game
+        // query to get info on this game and answers
         const game = await db.Game.findByPk(gameId);
 
         // define variables
@@ -74,10 +135,8 @@ export default function initGamesController(db) {
         // set default color of letters to be displayed
         const letterCheck = ['in-position', 'in-position', 'in-position', 'in-position', 'in-position'];
 
-        // case where guess is correct
+        // case where guess is correct, update game in game DB
         if (word === guess) {
-          //
-          // update game with new info
           await game.update({
             gameState: {
               words: game.gameState.words,
@@ -87,7 +146,6 @@ export default function initGamesController(db) {
               color: [...colorArray, letterCheck],
             },
           });
-
           // change response object
           responseObj = {
             isWord: true,
@@ -97,17 +155,16 @@ export default function initGamesController(db) {
             split: guessSplit,
           };
         }
-        // determine whether letters are 'in-word', 'in-position', 'not-in-word'
 
-        // find all the letters that are 'in-position' first and remove from tally
-        // we should always consider the in-position words first
+        // case where guess is wrong, update color codes for words
         else {
+          // do a tally of the letters in users guess
           for (let j = 0; j < guessSplit.length; j += 1) {
             if (guessSplit[j] === wordSplit[j]) {
               tally[guessSplit[j]] -= 1;
             }
           }
-          // compare to see if the letters are 'in-word' against updated tally.
+          // compare to users guess tally vs answer tally and update color
           for (let i = 0; i < guessSplit.length; i += 1) {
             if (guessSplit[i] !== wordSplit[i]) {
               if (guessSplit[i] in tally && tally[guessSplit[i]] > 0) {
@@ -117,7 +174,7 @@ export default function initGamesController(db) {
             }
           }
 
-          // update game with new info
+          // update game DB with new info
           await game.update({
             gameState: {
               words: game.gameState.words,
@@ -144,63 +201,20 @@ export default function initGamesController(db) {
     }
   };
 
-  // check unique code
-  const checkCodeAndCreate = async (request, response) => {
-    try {
-      // check if code is in Wordle DB
-      const { enteredCode } = request.body;
-      const { user } = request.body;
-      const wordle = await db.Wordle.findOne({ where: { code: `${enteredCode}` } });
-
-      // define base response object
-      let responseObj = {
-        availCode: false,
-      };
-      if (wordle) {
-        const existingGame = await db.Game.findOne({ where: { playerId: `${user}`, wordleId: `${wordle.id}` } });
-        if (!existingGame) {
-          // define the new game to be created
-          const newGame = {
-            wordleId: wordle.id,
-            gameState: {
-              words: wordle.words.words,
-              tally: wordle.words.tally,
-              currentWord: 0,
-              guesses: [],
-              color: [],
-            },
-            playerId: user,
-          };
-          // create game in DB
-          const game = await db.Game.create(newGame);
-          responseObj = {
-            availCode: true,
-            gameId: game.id,
-          };
-        } else {
-          responseObj = {
-            availCode: true,
-            gameId: existingGame.id,
-          };
-        }
-      }
-      // send info back to client
-      response.send(responseObj);
-    } catch (error) {
-      response.status(500).send(error);
-    }
-  };
-
-  // check guess against word
+  /**
+ * @desc for user to move on to the next word in the wordle
+ * @param request game ID
+ */
   const goToNextWord = async (request, response) => {
     try {
       const { gameId } = request.body;
-      // query the word for this game
+      // get info about game from DB
       const game = await db.Game.findByPk(gameId);
 
-      // define variables
       const numOfWords = game.gameState.words.length;
+      // move game to the next word
       let newWordNum = Number(game.gameState.currentWord) + 1;
+      // if user is already at the last word, go back to the first word
       if (newWordNum == numOfWords) { newWordNum = 0; }
 
       // update game with new info
@@ -214,8 +228,8 @@ export default function initGamesController(db) {
         },
       });
 
+      // get wordle info for display
       const wordle = await db.Wordle.findByPk(game.wordleId);
-
       response.send(
         {
           name: wordle.name,
@@ -228,14 +242,17 @@ export default function initGamesController(db) {
     }
   };
 
-  // check guess against word
+  /**
+ * @desc when user has used up all 6 guesses, to allow them to reset and try again
+ * @param request game ID
+ */
   const resetGame = async (request, response) => {
     try {
       const { gameId } = request.body;
-      // query the word for this game
+      // get info about game from DB
       const game = await db.Game.findByPk(gameId);
 
-      // update game with new info
+      // reset guesses and color
       await game.update({
         gameState: {
           words: game.gameState.words,
@@ -246,8 +263,8 @@ export default function initGamesController(db) {
         },
       });
 
+      // get wordle info for display
       const wordle = await db.Wordle.findByPk(game.wordleId);
-
       response.send(
         {
           name: wordle.name,
@@ -259,6 +276,7 @@ export default function initGamesController(db) {
       response.status(500).send(error);
     }
   };
+
   return {
     checkGuess,
     checkCurrentGameState,
